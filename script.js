6d1ceefdb656bcad45deb55eeea4715a6d0b15da
amir0707k <34520003+amir0707k@users.noreply.github.com>
2023-08-30 18:29:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+async function promise(){
+       return await new Promise((resolve, reject) => {
+          setTimeout(() => {
+            resolve("Hello, world!");
+          }, 1000);
+        });
+      }
+      promise().then((data) => {
+        document.getElementById("output").innerText = data;
+      })
